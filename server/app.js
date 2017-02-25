e4a8d4be82b1af1bd5d114c6be2b426113a4a54d
Shawn McLaughlin <shawnmclau@gmail.com>
2017-02-25 13:44:27 -0500

    config for auth modules in app.js

--- server/app.js
+++ server/app.js
@@ -5,6 +5,12 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+//auth modules
+let session = require('express-session');
+let passport = require('passport');
+let passportlocal = require('passport-local');
+let LocalStrategy = passportlocal.Strategy;
+let flash = require('connect-flash'); //display errors/login messages
 
 // import "mongoose" - required for DB Access
 let mongoose = require('mongoose');
@@ -16,7 +22,7 @@ mongoose.connect(process.env.URI || config.URI);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log("Conneced to MongoDB...");
+    console.log("Conneced to MongoDB...");
 });
 
 // define routers
@@ -42,22 +48,46 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', index);
 app.use('/books', books);
 
+// setup sessions
+app.use(session({
+    secret: "SeizeTheMeansOfProduction",
+    saveUninitialized: true,
+    resave: true
+}));
+
+//initialize passport and flash
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/', index);
+app.use('/games', games);
+
+//Passport User Configuration
+let UserModel = require('./models/users');
+let User = UserModel.User;
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 // Handle 404 Errors
-  app.use(function(req, res) {
-      res.status(400);
-     res.render('errors/404',{
-      title: '404: File Not Found'
+app.use(function(req, res) {
+    res.status(400);
+    res.render('errors/404', {
+        title: '404: File Not Found'
     });
-  });
+});
 
-  // Handle 500 Errors
-  app.use(function(error, req, res, next) {
-      res.status(500);
-      res.render('errors/500', {
-        title:'500: Internal Server Error',
+// Handle 500 Errors
+app.use(function(error, req, res, next) {
+    res.status(500);
+    res.render('errors/500', {
+        title: '500: Internal Server Error',
         error: error
-      });
-  });
+    });
+});
 
 module.exports = app;
